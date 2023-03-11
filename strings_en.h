7d55c18392d82f4e3439b9426792aa2a4f6d2dd5
danielbernalb <danielbernalb@gmail.com>
2023-03-11 11:58:09 -0500

    Minor change

--- strings_en.h
+++ strings_en.h
@@ -65,8 +65,8 @@ const char HTTP_FORM_WIFI_END[]    PROGMEM = "";
 const char HTTP_FORM_STATIC_HEAD[] PROGMEM = "<hr><br/>";
 const char HTTP_FORM_END[]         PROGMEM = "<br/><br/><button type='submit'>Save</button></form>";
 const char HTTP_FORM_LABEL[]       PROGMEM = "<label for='{i}'>{t}</label>";
-const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "<hr><br/>";
-//const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "";
+//const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "<hr><br/>";
+const char HTTP_FORM_PARAM_HEAD[]  PROGMEM = "";
 const char HTTP_FORM_PARAM[]       PROGMEM = "<br/><input id='{i}' name='{n}' maxlength='{l}' value='{v}' {c}>\n"; // do not remove newline!
 
 const char HTTP_SCAN_LINK[]        PROGMEM = "<br/><form action='/wifi?refresh=1' method='POST'><button name='refresh' value='1'>Refresh</button></form>";
